35403ede76b806f61e58b70e00b1760dbe4c37f0
captainharshal <captainharshal@gmail.com>
2022-12-01 20:00:40 +0530

    new

---
+++ pythonProject1/addDemo.py
@@ -0,0 +1,5 @@
+def numadd(a,b):
+    print("A is :", a)
+    print("B is :", b)
+    print("ADDITION is :", a+b)
+    print("This is ADD DEMO")
---
+++ pythonProject1/swapDemo1.py
@@ -0,0 +1,19 @@
+def numswap1(a, b):
+    print("A is :", a)
+    print("B is :", b)
+    a=a+b
+    b=a-b
+    a=a-b
+    print("A is :", a)
+    print("B is :", b)
+    print ("This is MaxNumber2")
+
+
+# def numswap1(a, b):
+#     print("A is :", a)
+#     print("B is :", b)
+#     a, b = b, a
+#     print("A is :", a)
+#     print("B is :", b)
+#
+# numswap1(5, 2)
